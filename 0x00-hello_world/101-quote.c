20a2843fc3e04e48fce2ab425f54f05968800d3c
Samuel <ogboyesam@gmail.com>
2023-03-16 20:14:26 -0700

    1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main-  a C program that prints exactly and that piece of art is useful" -
+  *Dora Korpar, 2015-10-19, followed by a new line, to the standard error
+  * Return: 1 (Success)
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      stdout);
+	return 1;
+}
