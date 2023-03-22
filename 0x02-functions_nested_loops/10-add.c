9846c7d585cb055425a713d461ae662ee353dca4
Samuel <ogboyesam@gmail.com>
2023-03-21 17:54:52 -0700

    a program to add two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add- a function that adds two integers and returns the result
+ * @i: intergers to add together
+ * @j: intergers to add together
+ * Return: the result
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	_putchar(sum + '0');
+	return (sum);
+}
