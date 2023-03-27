f686a66f3894b1a3ce88a6b34d9b29543831cc1e
Samuel <ogboyesam@gmail.com>
2023-03-27 14:59:07 -0700

    A function to swap the value of teo integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the values of two integers
+ * @a: input
+ * @b: input
+ * Return: 0 Success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
