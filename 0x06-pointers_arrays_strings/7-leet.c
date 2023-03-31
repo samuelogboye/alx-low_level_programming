85c5e5cbdbb328920af6069cae7052280816801c
Samuel <ogboyesam@gmail.com>
2023-03-30 17:27:03 -0700

    a programe to encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+*leet- a function that encodes a string into 1337
+*@str: string to be encoded
+*
+*Return: pointer to the encoded string
+*/
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+		i++;
+	}
+	return (str);
+}
