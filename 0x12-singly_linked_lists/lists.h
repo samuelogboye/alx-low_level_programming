ac63954df68fa01f548c3f7e7139f1a23dbf2a12
Samuel <ogboyesam@gmail.com>
2023-04-28 17:03:44 -0700

    A function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 /*
  * File Name: lists.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
-#include <stdio.h?>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -35,6 +35,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_str(void)__attribute__((constructor))
+void print_str(void)__attribute__((constructor));
 
 #endif /* LIST_H*/
