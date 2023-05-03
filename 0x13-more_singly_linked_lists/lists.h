affc5b765ffb9a6e06af1601dad1de5faaea80a3
Samuel <ogboyesam@gmail.com>
2023-05-02 21:07:34 -0700

    Print safe version

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,4 +37,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
+size_t check_loop(const listint_t *head);
 #endif /* LIST_H */
