0f16b45da3f608a831cb201455b8f8feb2771474
Samuel <ogboyesam@gmail.com>
2023-05-04 21:51:05 -0700

    To get the bit at nth position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * get_bit - function that returns the value of a bit at a given index
+  * @n: bit value
+  * @index: the value of the bit
+  *
+  * Return: the value of the bit at index index or -1 if an error occured
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 32)
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+}
