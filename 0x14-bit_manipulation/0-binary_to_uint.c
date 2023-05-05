0d16616685f7a4c20c1c8cfa488214bcb99e88cc
Samuel <ogboyesam@gmail.com>
2023-05-04 21:23:01 -0700

    A function that converts binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a function that converts a binary number to an unsigned int
+ * @b: binary to be converted
+ * Return: the converted number or 0 if there is one or more chars in the
+ * string b that is not 0 or 1, and b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+
+		num <<= 1;
+		if (b[i] == '1')
+		{
+			num += 1;
+		}
+	}
+	return (num);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,6 @@
-l
+l0x14. C - Bit manipulation
+
+Learning Objectives
+Understanding how to manipulate bits and use bitwise operators
+Binary
+Hexadecimal
