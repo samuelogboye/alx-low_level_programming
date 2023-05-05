69b6a26900a63b48d856863a3782aa62c19c18ac
Samuel <ogboyesam@gmail.com>
2023-05-04 21:47:19 -0700

    converts integer to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number
+ * @n: integer to be converted
+ * Return: the binary
+ */
+void print_binary(unsigned long int n)
+{
+	int count = 0;
+	unsigned long int notation = n;
+
+	while (notation)
+	{
+		notation >>= 1;
+		count++;
+	}
+
+	if (!count)
+	{
+		_putchar('0');
+	}
+
+	while (count)
+	{
+		notation = n >> --count;
+		if (notation & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
