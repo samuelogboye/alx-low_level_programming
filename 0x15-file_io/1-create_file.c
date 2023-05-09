68271a5ce7100dfd6c4143cdcbc82c807925a67c
Samuel <ogboyesam@gmail.com>
2023-05-08 22:28:06 -0700

    task 1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,28 +8,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, i;
+	int fd, len, i = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 	{
 		return (-1);
 	}
 
 
-	if (!text_content)
+	if (text_content == NULL)
 	{
 		text_content = "";
 	}
 
-	for (i = 0; text_content[i]; i++)
+	while (text_content != NULL && text_content[i])
 	{
-		;
+		i++;
 	}
 
 	len = write(fd, text_content, i);
