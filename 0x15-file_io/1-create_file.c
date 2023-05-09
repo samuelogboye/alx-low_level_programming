bf70907b07f03f3819b07430329a7346da996b5d
Samuel <ogboyesam@gmail.com>
2023-05-08 21:57:04 -0700

    A function to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 
-	if (text_content != NULL)
+	if (!text_content)
 	{
 		text_content = "";
 	}
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	}
 
-	len = write(STDOUT_FILENO, text_content, i);
+	len = write(fd, text_content, i);
 	if (len < 0)
 	{
 		return (-1);
