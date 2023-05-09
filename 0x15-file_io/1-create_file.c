ee9085868095c2c965ed78f4ce184e1cb50f98b9
Samuel <ogboyesam@gmail.com>
2023-05-08 21:54:06 -0700

    A function to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,15 +8,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len;
+	int fd, len, i;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 	{
 		return (-1);
@@ -25,13 +24,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		len = write(STDOUT_FILENO, text_content, sizeof(text_content));
-		if (len < 0)
-		{
-			close(fd);
-			return (-1);
-		}
+		text_content = "";
 	}
+
+	for (i = 0; text_content[i]; i++)
+	{
+		;
+	}
+
+	len = write(STDOUT_FILENO, text_content, i);
+	if (len < 0)
+	{
+		return (-1);
+	}
+
 	close(fd);
 	return (1);
 }
