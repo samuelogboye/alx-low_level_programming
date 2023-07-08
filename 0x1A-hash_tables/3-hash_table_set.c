889dec7d9ec618bf05857aa156ac645ba00fc2ac
Samuel <ogboyesam@gmail.com>
2023-07-07 18:49:22 -0700

    a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeds or 0 if otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size); /*the index */
+
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	{
+		return (0); /* Invalid input parameter */
+	}
+
+
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+
+	/* duplicate the key and value strings */
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	/* Handle collision, add new node at the begining of the linked list */
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
