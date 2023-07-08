49fddb1af8c7cf251d2f6f3c033120e9cb83441b
Samuel <ogboyesam@gmail.com>
2023-07-07 19:03:44 -0700

    a function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,54 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - a function that adds an element to the hash table
- * @ht: the hash table to add or update the key/value to
- * @key: the key
- * @value: the value associated with the key
- * Return: 1 if it succeeds or 0 if otherwise
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new;
 	char *value_copy;
-	hash_node_t *new_node;
-	unsigned long int index;
+	unsigned long int index, i;
 
-	index = key_index((const unsigned char *)key, ht->size); /*the index */
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
-	{
-		return (0); /* Invalid input parameter */
-	}
-
-
-	value_copy =  strdup(value);
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
-	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
 		free(value_copy);
 		return (0);
 	}
-
-	/* duplicate the key and value strings */
-	new_node->key = strdup(key);
-	new_node->value = value_copy;
-
-	if (new_node->key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (0);
 	}
-	/* Handle collision, add new node at the begining of the linked list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
